19ea2acadd4226206ad9ff3aab3b3568d090a608
DuanJiaNing <aimeimeits@gmail.com>
2020-01-18 23:17:47 +0800

    deploy

--- other/script/deploy-topic.sh
+++ other/script/deploy-topic.sh
@@ -1,4 +1,4 @@
-NAME=service-topic-impl-1.0-SNAPSHOT.jar
+NAME=topic-impl-1.0-SNAPSHOT.jar
 echo $NAME
 ID=`ps -ef | grep "$NAME" | grep -v "grep" | awk '{print $2}'`
 echo $ID
@@ -10,6 +10,7 @@ do
 done
 echo "prepare to deploy $NAME ..."
 
-java -Xmx256m -jar /usr/duan/jenkins/service/jar/service-topic-impl-1.0-SNAPSHOT.jar > /usr/duan/jenkins/service/log/service-topic.log 2>&1 &
+java -Xmx256m -jar /usr/duan/jenkins/service/jar/topic-impl-1.0-SNAPSHOT.jar > /usr/duan/jenkins/service/log/service-topic.log 2>&1 &
+java -Xmx256m -jar /usr/duan/vote/topic-impl-1.0-SNAPSHOT.jar > /usr/duan/vote/topic.log 2>&1 &
 
 echo excute success from deploy-topic.sh
